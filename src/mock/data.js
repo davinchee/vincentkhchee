292dbcb5dc1bd3fcdfd8e5a3d06d81dba1dea20d
Vincent Chee <vincent.kh.chee@gmail.com>
2020-06-16 04:07:05 -0400

    wip
    - resume text edit
    - footer stuff removed
    - data added

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,10 +9,10 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'hi, my name is',
+  name: 'vincent',
+  subtitle: "i'm a dev",
+  cta: 'more',
 };
 
 // ABOUT DATA
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1y94jxQE1ExAbBj-4dssYi_kGmoNAVDgg/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -57,9 +57,9 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'want to chat?',
+  btn: 'contact me',
+  email: 'thechee514@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +68,27 @@ export const footerData = {
     {
       id: uuidv1(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/chee_kh_v',
     },
     {
       id: uuidv1(),
       name: 'codepen',
-      url: '',
+      url: 'https://codepen.io/davinchee',
     },
     {
       id: uuidv1(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/vincent-kh-chee/',
     },
     {
       id: uuidv1(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/davinchee',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
